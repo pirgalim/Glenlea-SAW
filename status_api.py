2a83e5673c0861b79f108ce7d720724cc70b01b0
pirgalim <109841774+pirgalim@users.noreply.github.com>
2024-11-21 22:33:18 -0600

    raw status

--- status_api.py
+++ status_api.py
@@ -11,15 +11,24 @@ app = Flask(__name__)
 def status():    
     try:
         
+        status = dict()
+        
         print("Connecting to PWI4...")
 
         pwi4 = PWI4()
         s = pwi4.status()
         
-        # message = "Mount connected:", s.mount.is_connected
-        # print(message)
+        # connect = "Mount connected:", s.mount.is_connected
+        
+        
+        # status['mount'] = s.mount.is_connected
+        
+        # status['time'] = s.response.timestamp
+        
+
+        status = s.raw
             
-        return jsonify(s)
+        return jsonify(status)
         
     except:
         return jsonify('An error has occurred')

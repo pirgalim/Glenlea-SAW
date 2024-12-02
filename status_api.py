717bf4f0701cefb58054df596b766fe5ac346f4c
pirgalim <109841774+pirgalim@users.noreply.github.com>
2024-12-01 22:49:04 -0600

    code formatting

--- status_api.py
+++ status_api.py
@@ -2,13 +2,10 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 from pwi4_client import PWI4
 
-
 PORT = 5005
-
 
 app = Flask(__name__)
 CORS(app)
-
 
 @app.route('/status', methods=['GET'])
 def status():    
@@ -16,12 +13,9 @@ def status():
         print("Connecting to PWI4...")
         pwi4 = PWI4()
         s = pwi4.status()            
-        return jsonify(s.raw)
-
-        
+        return jsonify(s.raw)    
     except:
         return jsonify('Unable to connect to PWI4...')
-
-
+    
 if __name__ == '__main__':
    app.run(host='0.0.0.0', port=PORT)
